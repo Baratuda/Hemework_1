02ede6e7f1203e37c770ce561034cd7910f5e834
Egor Baranovskiy <egorpifagop@gmail.com>
2023-01-31 13:22:22 +0300

    Third example was done

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Please, input number to check it for parity: ");
+int number = int.Parse(Console.ReadLine());
+int privateNumbeer = (number/2);
+
+if(number-privateNumbeer*2==1 || 
+   number-privateNumbeer*2==-1){
+    Console.Write($"Number {number} isn't even");
+} else {
+    Console.Write($"Number {number} is even");
+}
+
