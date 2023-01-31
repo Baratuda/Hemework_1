92616e8601541eae488b5f59e25adcb7c3074dfe
Egor Baranovskiy <egorpifagop@gmail.com>
2023-01-31 12:25:29 +0300

    First example was did

---
+++ 0/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите чилсо A ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Введите чилсо B ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int maxNumber = secondNumber;
+if(firstNumber>secondNumber){
+    maxNumber = firstNumber;
+    Console.Write($"Max number is:  {firstNumber}; "+$"Min number is:  {secondNumber}; ");
+} else if(firstNumber==secondNumber){
+   Console.Write("These numbers equals each other");
+} else{
+   Console.Write($"Min number is:  {firstNumber}; "+$"Max number is:  {secondNumber}; ");
+}
